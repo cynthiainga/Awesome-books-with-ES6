91e798da71929c03f64180eda5b53fa581eaf7b0
Cynthia Inga <candyinga67@gmail.com>
2022-02-15 00:35:34 +0200

    Fix linters errors

--- modules/displayBooks.js
+++ modules/displayBooks.js
@@ -1,18 +1,18 @@
-import Book from "./Book.js";
+import Book from './Book.js';
 
 const books = new Book();
 
 const displayBooks = () => {
-  const booksContainer = document.getElementById("books-list");
+  const booksContainer = document.getElementById('books-list');
   books.refresh();
   const booksList = books.getList();
-  booksContainer.innerHTML = "";
+  booksContainer.innerHTML = '';
   for (let i = 0; i < booksList.length; i += 1) {
-    const book = document.createElement("li");
+    const book = document.createElement('li');
     book.innerHTML = `<h3 class="book">"${booksList[i].title}" by ${booksList[i].author}</h3>
                       <button id="book${i}" type="button">Remove</button>`;
     booksContainer.appendChild(book);
-    document.getElementById(`book${i}`).addEventListener("click", () => {
+    document.getElementById(`book${i}`).addEventListener('click', () => {
       books.remove(i);
       displayBooks();
     });
